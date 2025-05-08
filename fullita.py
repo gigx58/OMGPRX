1cfb944cf13c5aeb2c391f2c361a6ae1e4209701
gigx58 <chicco.stranieri@libero.it>
2025-05-08 20:21:01 +0200

    fullita.py

--- fullita.py
+++ fullita.py
@@ -12,8 +12,8 @@ import time
 
 # Constants
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://gigx58-mazztvmediaprox.hf.space"
+PASSWORD = "58"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
